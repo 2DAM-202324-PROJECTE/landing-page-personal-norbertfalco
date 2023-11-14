4df1fb100d0486318fd4dd5606bd9100c1c57dbe
Norbert Falcó <norbertfalco@gmail.com>
2023-11-14 18:38:30 +0100

    Arreglat efecte particules

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import Alpine from 'alpinejs'
 
+
 window.Alpine = Alpine
 
 Alpine.start()
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,27 @@
+import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+
 export default {
+    resolve: {
+        alias: [
+            //alias per referenciar en html una carpeta particlesjs quan en realitat està en node_modules/paticles.js. Permet mantenir el mateix path en dev i prod.
+            {find: /^particlesjs/, replacement: 'node_modules/particles.js'}
+        ]
+    },
+    // config options
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {   //copia un js que no es pot funcionar com Module a una carpeta destí que s'ubicarà en dist
+                    src: 'node_modules/particles.js/particles.js',
+                    dest: 'particlesjs'
+                }
+            ]
+        })
+    ],
     // config options
     base: '/landing-page-personal-norbertfalco/'
 }
+
+
